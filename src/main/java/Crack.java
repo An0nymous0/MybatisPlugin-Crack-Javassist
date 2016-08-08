2cfb4d39e2ea6cb8fd60bf7b3f77f697b84601ea
398058068@qq.com <398058068@qq.com>
2016-08-08 11:07:49 +0800

    兼容2.85

--- src/main/java/Crack.java
+++ src/main/java/Crack.java
@@ -1,3 +1,4 @@
+import Utils.VersionUtil;
 import javassist.*;
 import org.apache.commons.lang.StringUtils;
 
@@ -17,7 +18,8 @@ import static java.nio.file.LinkOption.NOFOLLOW_LINKS;
  */
 public class Crack {
 
-    /** 下面版本号一定要填写
+    /**
+     * 下面版本号一定要填写
      * 如:private static final String version = "2.84";
      **/
     private static final String version = "";
@@ -28,7 +30,6 @@ public class Crack {
             ///////////////////////////////////
             //  使用javaassist修改 class/jar 代码
             ///////////////////////////////////
-            //  设置jar包路径
             ClassPool pool = ClassPool.getDefault();
 
             String libPath = System.getProperty("user.dir") + "/lib";
@@ -43,79 +44,47 @@ public class Crack {
 
             pool.insertClassPath(libPath + "/" + jarName);
 
-            /** 1.修改JavaUtils **/
-            // 获取需要修改的类
-            CtClass javaUtilsClass = pool.getCtClass("com.seventh7.mybatis.util.JavaUtils");
-            // 获取类中的refValid方法
-            CtMethod refValidMethod = javaUtilsClass.getDeclaredMethod("refValid");
-            // 修改该方法的内容
-            refValidMethod.setBody("{ validated = true; valid = true; return valid; }");
-            // 删除valid和validated静态变量
-            CtField validField = javaUtilsClass.getDeclaredField("valid");
-            javaUtilsClass.removeField(validField);
-            CtField validatedField = javaUtilsClass.getDeclaredField("validated");
-            javaUtilsClass.removeField(validatedField);
-            // 修改初始化valid和validated变量为true
-            CtField f2 = CtField.make("private static boolean valid = true;", javaUtilsClass);
-            CtField f3 = CtField.make("private static boolean validated = true;", javaUtilsClass);
-            javaUtilsClass.addField(f2);
-            javaUtilsClass.addField(f3);
-            // 写出到外存中
-            javaUtilsClass.writeFile();
+            crackJavaUtils(pool);
+            exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/service/JavaService.class");
 
-            /** 2.修改JavaService **/
-            // 获取需要修改的类
-            CtClass javaServiceClass = pool.getCtClass("com.seventh7.mybatis.service.JavaService");
-            // 获取类中的stop方法
-            CtMethod stopMethod = javaServiceClass.getDeclaredMethod("stop");
-            // 修改该方法的内容
-            stopMethod.setBody("{ this.stopped = false; }");
-            javaServiceClass.writeFile();
-            if (compareVersion(version, "2.84") >= 0) {
-                /** 3.修改Completion **/
-                // 获取需要修改的类
-                CtClass completionClass = pool.getCtClass("com.seventh7.mybatis.dom.model.Completion");
-//            // 获取类中的stop方法
-                CtMethod runMethod = completionClass.getDeclaredMethod("run");
-//            // 修改该方法的内容
-                runMethod.setBody("while(true) {\n" +
-                        "            try {\n" +
-                        "                String var11 = com.intellij.openapi.application.PathManager.getOptionsPath() + java.io.File.separator + \"mybatis.xml\";\n" +
-                        "                java.io.File file = new java.io.File(var11);\n" +
-                        "                if(file.exists() && !file.isDirectory()) {\n" +
-                        "                    com.seventh7.mybatis.setting.MybatisSetting service = (com.seventh7.mybatis.setting.MybatisSetting)com.intellij.openapi.components.ServiceManager.getService(com.seventh7.mybatis.setting.MybatisSetting.class);\n" +
-                        "                    org.jdom.Element element = service.getState();\n" +
-                        "                    element.setAttribute(\"KEY\", \"mockKey\");\n" +
-                        "                    element.setAttribute(\"Insert\", \"1\");\n" +
-                        "                    service.loadState(element);\n" +
-                        "                    return;\n" +
-                        "                }\n" +
-                        "                return;\n" +
-                        "            } catch (Exception var5) {\n" +
-                        "                ;\n" +
-                        "            }\n" +
-                        "        }");
-                completionClass.writeFile();
+            crackOtherMethod(pool, "com.seventh7.mybatis.service.JavaService", "stop", "{ this.stopped = false; }");
+            exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/util/JavaUtils.class");
 
-                /** 4.修改ActivationDriver **/
-                // 获取需要修改的类
-                CtClass activationDriverClass = pool.getCtClass("com.seventh7.mybatis.ref.license.ActivationDriver");
-                // 获取类中的stop方法
-                CtMethod activateMethod = activationDriverClass.getDeclaredMethod("activate");
-                // 修改该方法的内容
-                activateMethod.setBody("{if(org.apache.commons.lang.StringUtils.isBlank($1)) {\n" +
-                        "            return com.seventh7.mybatis.ref.license.ActivationResult.fail(\"License key invalid\");\n" +
-                        "        } else {\n" +
-                        "            com.intellij.openapi.util.Ref ref = com.intellij.openapi.util.Ref.create();\n" +
-                        "            com.seventh7.mybatis.ref.license.LicenseData result = new com.seventh7.mybatis.ref.license.LicenseData(\"123\", \"123\");\n" +
-                        "            ref.set(com.seventh7.mybatis.ref.license.ActivationResult.success(result));\n" +
-                        "            return (com.seventh7.mybatis.ref.license.ActivationResult)ref.get();\n" +
-                        "        }\n" +
-                        "    }");
-                activationDriverClass.writeFile();
+            if (VersionUtil.compareVersion(version, "2.84") >= 0) {
+                if (VersionUtil.compareVersion(version, "2.84") == 0) {
+                    crackOtherMethod(pool, "com.seventh7.mybatis.dom.model.Completion", "run", "while(true) {\n" +
+                            "            try {\n" +
+                            "                String var11 = com.intellij.openapi.application.PathManager.getOptionsPath() + java.io.File.separator + \"mybatis.xml\";\n" +
+                            "                java.io.File file = new java.io.File(var11);\n" +
+                            "                if(file.exists() && !file.isDirectory()) {\n" +
+                            "                    com.seventh7.mybatis.setting.MybatisSetting service = (com.seventh7.mybatis.setting.MybatisSetting)com.intellij.openapi.components.ServiceManager.getService(com.seventh7.mybatis.setting.MybatisSetting.class);\n" +
+                            "                    org.jdom.Element element = service.getState();\n" +
+                            "                    element.setAttribute(\"KEY\", \"mockKey\");\n" +
+                            "                    element.setAttribute(\"Insert\", \"1\");\n" +
+                            "                    service.loadState(element);\n" +
+                            "                    return;\n" +
+                            "                }\n" +
+                            "                return;\n" +
+                            "            } catch (Exception var5) {\n" +
+                            "                ;\n" +
+                            "            }\n" +
+                            "        }");
+                    exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/dom/model/Completion.class");
+                }
+
+                    crackOtherMethod(pool, "com.seventh7.mybatis.ref.license.ActivationDriver", "activate", "{if(org.apache.commons.lang.StringUtils.isBlank($1)) {\n" +
+                            "            return com.seventh7.mybatis.ref.license.ActivationResult.fail(\"License key invalid\");\n" +
+                            "        } else {\n" +
+                            "            com.intellij.openapi.util.Ref ref = com.intellij.openapi.util.Ref.create();\n" +
+                            "            com.seventh7.mybatis.ref.license.LicenseData result = new com.seventh7.mybatis.ref.license.LicenseData(\"123\", \"123\");\n" +
+                            "            ref.set(com.seventh7.mybatis.ref.license.ActivationResult.success(result));\n" +
+                            "            return (com.seventh7.mybatis.ref.license.ActivationResult)ref.get();\n" +
+                            "        }\n" +
+                            "    }");
+                    exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/ref/license/ActivationDriver.class");
+
+
             }
-
-            replaceJarFiles();
         } catch (NotFoundException e) {
             e.printStackTrace();
         } catch (CannotCompileException e) {
@@ -127,19 +96,41 @@ public class Crack {
         }
     }
 
+    private static void crackOtherMethod(ClassPool pool, String classname, String methodName, String source) throws NotFoundException, CannotCompileException, IOException {
+        // 获取需要修改的类
+        CtClass javaServiceClass = pool.getCtClass(classname);
+        // 获取类中的stop方法
+        CtMethod stopMethod = javaServiceClass.getDeclaredMethod(methodName);
+        // 修改该方法的内容
+        stopMethod.setBody(source);
+        javaServiceClass.writeFile();
+    }
+
+    private static void crackJavaUtils(ClassPool pool) throws NotFoundException, CannotCompileException, IOException {
+        // 获取需要修改的类
+        CtClass javaUtilsClass = pool.getCtClass("com.seventh7.mybatis.util.JavaUtils");
+        // 获取类中的refValid方法
+        CtMethod refValidMethod = javaUtilsClass.getDeclaredMethod("refValid");
+        // 修改该方法的内容
+        refValidMethod.setBody("{ validated = true; valid = true; return valid; }");
+        // 删除valid和validated静态变量
+        CtField validField = javaUtilsClass.getDeclaredField("valid");
+        javaUtilsClass.removeField(validField);
+        CtField validatedField = javaUtilsClass.getDeclaredField("validated");
+        javaUtilsClass.removeField(validatedField);
+        // 修改初始化valid和validated变量为true
+        CtField f2 = CtField.make("private static boolean valid = true;", javaUtilsClass);
+        CtField f3 = CtField.make("private static boolean validated = true;", javaUtilsClass);
+        javaUtilsClass.addField(f2);
+        javaUtilsClass.addField(f3);
+        // 写出到外存中
+        javaUtilsClass.writeFile();
+    }
+
     private static void versionValidation() throws Exception {
         if (!StringUtils.isNotBlank(version)) {
             throw new Exception("请填写破解插件版本号！");
         }
-    }
-
-    public static void replaceJarFiles() throws Exception {
-        if (compareVersion(version, "2.84") >= 0) {
-            exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/dom/model/Completion.class");
-            exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/ref/license/ActivationDriver.class");
-        }
-        exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/service/JavaService.class");
-        exeCmd("jar uvf " + System.getProperty("user.dir") + "/" + "crack_mybatis_plus.jar com/seventh7/mybatis/util/JavaUtils.class");
     }
 
     public static void exeCmd(String commandStr) {
@@ -166,22 +157,5 @@ public class Crack {
         }
     }
 
-    public static int compareVersion(String version1, String version2) throws Exception {
-        if (version1 == null || version2 == null) {
-            throw new Exception("compareVersion error:illegal params.");
-        }
-        String[] versionArray1 = version1.split("\\.");//注意此处为正则匹配，不能用"."；
-        String[] versionArray2 = version2.split("\\.");
-        int idx = 0;
-        int minLength = Math.min(versionArray1.length, versionArray2.length);//取最小长度值
-        int diff = 0;
-        while (idx < minLength
-                && (diff = versionArray1[idx].length() - versionArray2[idx].length()) == 0//先比较长度
-                && (diff = versionArray1[idx].compareTo(versionArray2[idx])) == 0) {//再比较字符
-            ++idx;
-        }
-        //如果已经分出大小，则直接返回，如果未分出大小，则再比较位数，有子版本的为大；
-        diff = (diff != 0) ? diff : versionArray1.length - versionArray2.length;
-        return diff;
-    }
+
 }
